ebcc22eefebd67b22372460a44b5bbbbafb5acb1
David Cady <cadykd@gmail.com>
2022-06-21 10:05:38 -0700

    Adding current work to GitHub

---
+++ src/main/java/com/CadyKD/KDGames/KDGamesApplication.java
@@ -0,0 +1,13 @@
+package com.CadyKD.KDGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KDGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KDGamesApplication.class, args);
+	}
+
+}
